df129017fdd9be1198abeb307e5cb37d4ba2392a
Colin <colin@thinkr.fr>
2017-09-08 14:23:06 +0200

    changes in emphasise

--- R/emphasize.R
+++ R/emphasize.R
@@ -3,6 +3,8 @@
 #' Emphasize a selected text.
 #'
 #' @return a backticked word
+#' 
+#' @rdname emphasize
 #' @export
 #'
 
@@ -11,10 +13,16 @@ italicsr <- function(){
   rstudioapi::insertText(location = a$selection[[1]]$range, text = paste0("_", a$selection[[1]]$text, "_"))
 }
 
+#' @rdname emphasize
+#' @export
+
 boldr <- function(){
   a <- rstudioapi::getSourceEditorContext()
   rstudioapi::insertText(location = a$selection[[1]]$range, text = paste0("__", a$selection[[1]]$text, "__"))
 }
+
+#' @rdname emphasize
+#' @export
 
 strikr <- function(){
   a <- rstudioapi::getSourceEditorContext()
