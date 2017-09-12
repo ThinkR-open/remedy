2b853db674f279b7e55bf74a3331cc05e729d3cd
Colin <colin@thinkr.fr>
2017-09-12 16:19:59 +0200

    fixed bug in downr, close #5

--- R/mover.R
+++ R/mover.R
@@ -29,12 +29,18 @@ rightr <- function(){
 #' 
 downr <- function(){
   a <- rstudioapi::getSourceEditorContext()
+  
+  content <- a$contents
+  
   rg <- a$selection[[1]]$range
-  pos <- a$selection[[1]]$range[[1]][[1]]
-  ncol <- rg$end[[2]] - rg$start[[2]]
-  rg$start[[1]] <- rg$start[[1]] + 1
-  rg$end[[1]] <- rg$end[[1]] + 1
-  rstudioapi::insertText(location = rg, text = "\n")
+  start <- a$selection[[1]]$range[[1]][[1]]
+  end <- a$selection[[1]]$range[[2]][[1]]
+  nline <- rg$end[[1]] - rg$start[[1]]
+  
+  fin <- content[(end+1):length(content)]
+  
+  rg$start[[1]] <- rg$end[[1]] + 1
+  rg$end[[1]] <- rg$end[[1]] + nline + 1
   # If text is selected
   if (nchar(a$selection[[1]]$text) != 0) {
     rstudioapi::insertText(location = rg, text = a$selection[[1]]$text)
@@ -43,5 +49,8 @@ downr <- function(){
     rstudioapi::insertText(location = rg, text = a$contents[pos])
     rstudioapi::setCursorPosition(rg)
   }
-}
+  rg$start[[1]] <- rg$end[[1]] + 1
+  rg$end[[1]] <- rg$end[[1]] + nline
 
+  rstudioapi::insertText(location = rg, text = paste0(fin, collapse = "\n"))
+}
