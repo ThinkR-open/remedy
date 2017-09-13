3ef6f361498dd98d9f22435a6471093025d6f3af
Colin <colin@thinkr.fr>
2017-09-13 13:57:41 +0200

    moved downr for experimenting in another branch

--- R/mover.R
+++ R/mover.R
@@ -23,34 +23,3 @@ rightr <- function(){
   }
 }
 
-#' @rdname move
-#' 
-#' @export
-#' 
-downr <- function(){
-  a <- rstudioapi::getSourceEditorContext()
-  
-  content <- a$contents
-  
-  rg <- a$selection[[1]]$range
-  start <- a$selection[[1]]$range[[1]][[1]]
-  end <- a$selection[[1]]$range[[2]][[1]]
-  nline <- rg$end[[1]] - rg$start[[1]]
-  
-  fin <- content[(end+1):length(content)]
-  
-  rg$start[[1]] <- rg$end[[1]] + 1
-  rg$end[[1]] <- rg$end[[1]] + nline + 1
-  # If text is selected
-  if (nchar(a$selection[[1]]$text) != 0) {
-    rstudioapi::insertText(location = rg, text = a$selection[[1]]$text)
-    rstudioapi::setCursorPosition(rg)
-  } else {
-    rstudioapi::insertText(location = rg, text = a$contents[pos])
-    rstudioapi::setCursorPosition(rg)
-  }
-  rg$start[[1]] <- rg$end[[1]] + 1
-  rg$end[[1]] <- rg$end[[1]] + nline
-
-  rstudioapi::insertText(location = rg, text = paste0(fin, collapse = "\n"))
-}
