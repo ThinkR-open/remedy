4e287a50f959b2f69cdc9d97bdfffaddd40baf8e
Jonathan Sidi <yonicd@gmail.com>
2017-11-25 19:52:23 -0500

    add multiline support for chunk splitting

--- R/chunksplitr.R
+++ R/chunksplitr.R
@@ -1,6 +1,6 @@
 #' @title Split Chunk
 #' @description Split a chunk into two chunks
-#' @details Will split below row of the current cursor position.
+#' @details Will split below row of the current cursor position or under the hightled text.
 #' @rdname chunksplitr
 #' @export 
 #' @importFrom rstudioapi getSourceEditorContext insertText
@@ -8,8 +8,8 @@ chunksplitr <- function(){
   adc <- rstudioapi::getSourceEditorContext()
   
   rng <- rstudioapi::document_range(
-    start = rstudioapi::document_position(adc$selection[[1]]$range$start[1], Inf),
-    end = rstudioapi::document_position(adc$selection[[1]]$range$start[1], Inf)
+    start = rstudioapi::document_position(adc$selection[[1]]$range$end[1], Inf),
+    end = rstudioapi::document_position(adc$selection[[1]]$range$end[1], Inf)
     )
   
   rstudioapi::insertText(location = rng,
