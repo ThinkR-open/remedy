6b062acc90cba585ad7c51de2f720362b7a8393c
Daniel Possenriede <possenriede@gmail.com>
2018-07-02 23:01:25 +0200

    Add olistr: ordered list

--- R/listr.R
+++ R/listr.R
@@ -7,8 +7,18 @@
 #'  <img src = "https://github.com/ThinkR-open/remedy/blob/master/readme_gif/list.gif?raw=true">
 #' }}
 #'
-#' @return a markdown list
+#' @return an unordered markdown list
 #' @export
 #' @importFrom rstudioapi getSourceEditorContext insertText
 #' 
 listr <- function() add_multiline_prefix('+ ', as_is = FALSE)
+
+#' @title Convert to ordered list
+#'
+#' @description Turn the selected text into an ordered list.
+#'
+#' @return an ordered markdown list
+#' @export
+#' @importFrom rstudioapi getSourceEditorContext insertText
+#' @rdname listr
+olistr <- function() add_multiline_prefix('1. ', as_is = FALSE)
