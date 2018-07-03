f44870c74cb01dc133b577c741bcb05adf6253c8
Daniel Possenriede <possenriede@gmail.com>
2018-07-03 23:05:35 +0200

    add/update tests multiline_prefix, blockquoter, listr

--- tests/testthat/test-blockquote.R
+++ tests/testthat/test-blockquote.R
@@ -1,56 +1,53 @@
-testthat::context("blockquote")
+testthat::context("blockquoter")
 
-if(rstudioapi::isAvailable()){
-  
-  path <- tempfile(pattern = 'test',fileext = '.R')
-  file.create(path)
-  rstudioapi::navigateToFile(path)
-  Sys.sleep(1)
-  sec <- rstudioapi::getSourceEditorContext()
-  
-}
+sec <- scratch_file()
 
-testthat::describe('lists',{
-  
+testthat::describe("blockquoter", {
   skip_if_not_rstudio()
-  
-  it('empty',{
-    
+
+  it("empty", {
     blockquoter()
-    
+
     rstudioapi::documentSave(sec$id)
-    
-    testthat::expect_length(readLines(path,warn = FALSE),0)
-    
+
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), "> ")
+
     set_text(sec = sec)
   })
-  
-  it('highlighted',{
-    
-    set_text('some text',sec = sec, mark = TRUE)
-    
+
+  it("highlighted", {
+    set_text("some text", sec = sec, mark = entire_document)
+
     blockquoter()
-    
+
     rstudioapi::documentSave(sec$id)
-    
-    testthat::expect_equal(readLines(path,warn = FALSE),'> some text')
-    
+
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), "> some text")
+
     set_text(sec = sec)
   })
-  
-  it('multiline',{
-    
-    set_text('some text\nmore text',sec = sec, mark = TRUE)
-    
+
+  it("multiline", {
+    set_text("some text\nmore text", sec = sec, mark = entire_document)
+
     blockquoter()
-    
+
     rstudioapi::documentSave(sec$id)
-    
-    testthat::expect_equal(readLines(path,warn = FALSE),c('> some text','> more text'))
-    
+
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), c("> some text", "> more text"))
+
+    set_text(sec = sec)
+  })
+
+  it("multiparagraph", {
+    set_text("some text\n\nmore text", sec = sec, mark = entire_document)
+
+    blockquoter()
+
+    rstudioapi::documentSave(sec$id)
+
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), c("> some text", "> ", "> more text"))
+
     set_text(sec = sec)
   })
 })
-
-
-
---
+++ tests/testthat/test-multiline_prefix.R
@@ -0,0 +1,115 @@
+testthat::context("multiline prefix")
+
+sec <- scratch_file()
+
+
+testthat::describe('prefix', {
+  skip_if_not_rstudio()
+  
+  add_multiline_prefix <- remedy:::add_multiline_prefix
+  
+  prefix <- 'prefix '
+  
+  it('empty',{
+    res <- prefix
+    
+    add_multiline_prefix(prefix, as_is = FALSE)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_length(readLines(sec$path, warn = FALSE), 0)
+    
+    set_text(sec = sec)
+  })
+  
+  it('empty as_is',{
+    res <- prefix
+    
+    add_multiline_prefix(prefix, as_is = TRUE)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+  
+  txt <- 'some text'
+
+  it('highlighted',{
+    res <- paste0(prefix, txt)
+    
+    set_text(txt, sec = sec, mark = entire_document)
+    
+    add_multiline_prefix(prefix)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+  
+  txt <- c('some text', 'more text')
+  
+  it('multiple lines', {
+    res <- paste0(prefix, txt)
+    
+    set_text(paste0(txt, collapse = "\n"), sec = sec, mark = entire_document)
+    
+    add_multiline_prefix(prefix)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+  
+  txt <- c('some text', '', 'more text')
+  
+  it("multiple paragraphs", {
+    res <- paste0(c(prefix, '', prefix), txt)
+    
+    set_text(paste0(txt, collapse = "\n"), sec = sec, mark = entire_document)
+    
+    add_multiline_prefix(prefix, as_is = FALSE)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+  
+  it("multiple paragraphs as_is", {
+    res <- paste0(prefix, txt)
+    
+    set_text(paste0(txt, collapse = "\n"), sec = sec, mark = entire_document)
+    
+    add_multiline_prefix(prefix, as_is = TRUE)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+  
+  txt <- c('some text', '    more text')
+  
+  it("nested list", {
+    res <- c('prefix some text', '    prefix more text')
+
+    set_text(paste0(txt, collapse = "\n"), sec = sec, mark = entire_document)
+    
+    add_multiline_prefix(prefix, as_is = FALSE)
+    
+    rstudioapi::documentSave(sec$id)
+    
+    testthat::expect_equal(readLines(sec$path, warn = FALSE), res)
+    
+    set_text(sec = sec)
+  })
+})
+ 
