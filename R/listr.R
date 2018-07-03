c0fbbc918a276c6e322f5f40be632beaf2eb04f0
Daniel Possenriede <possenriede@gmail.com>
2018-07-03 23:00:33 +0200

    update listr docs

--- R/listr.R
+++ R/listr.R
@@ -1,23 +1,19 @@
-#' @title Convert to unordered list
+#' @title Convert to list
 #'
-#' @description Turn the selected text into an unordered list.
+#' @description Convert selected text into an (un)ordered list.
 #'
 #' \if{html}{
 #' \out{
 #'  <img src = "https://github.com/ThinkR-open/remedy/blob/master/readme_gif/list.gif?raw=true">
 #' }}
 #'
-#' @return an unordered markdown list
+#' @return \code{listr()} returns an unordered markdown list
 #' @export
 #' @importFrom rstudioapi getSourceEditorContext insertText
 #' 
 listr <- function() add_multiline_prefix('+ ', as_is = FALSE)
 
-#' @title Convert to ordered list
-#'
-#' @description Turn the selected text into an ordered list.
-#'
-#' @return an ordered markdown list
+#' @return \code{olistr()} returns an ordered markdown list
 #' @export
 #' @importFrom rstudioapi getSourceEditorContext insertText
 #' @rdname listr
