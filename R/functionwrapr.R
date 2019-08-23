f8e1d236783f1665546659339314085a61f93034
whtns <kevin.stachelek@gmail.com>
2019-08-23 16:43:01 -0700

    update functionwrapr roxygen2 according to contributor guide

--- R/functionwrapr.R
+++ R/functionwrapr.R
@@ -3,17 +3,18 @@
 #' @description Wrap selected text in a function call
 #'
 #' @return text wrapped with parentheses with cursor set at leading parenthesis
-#' 
-#' @rdname functionwrapr
 #' @export
+#' @importFrom rstudioapi setCursorPosition
 #' 
 #' @examples 
 #' \dontrun{
 #' remedy_example(
-#'     c( "wrapped in function call"), 
+#'     c("wrapped in function call"), 
 #'     functionwrapr
 #'     )
 #' }
+#' 
+#' 
 functionwrapr <- function(prefix = "(", postfix = ")"){
   a <- enclose(prefix, postfix)
   rstudioapi::setCursorPosition(a$selection[[1]][["range"]][[1]])
